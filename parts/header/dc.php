d8f9a247a2e70aa2cd288fa953d78b5f81d40a7a
patlockley <patrick.lockley@googlemail.com>
2016-03-09 12:38:15 +0000

    Removed \n

--- parts/header/dc.php
+++ parts/header/dc.php
@@ -3,11 +3,11 @@
 			global $post;
 			$user = get_userdata($post->post_author);
 			$cats = array_merge(antonine_get_categories($post->ID), antonine_get_tags($post->ID));
-			?><meta name = "DC.Creator" content = "<?PHP echo $user->user_nicename; ?>">\n<?PHP
-			?><meta name = "DC.Title" content = "<?PHP echo $post->post_title; ?>\n<?PHP
-			?><meta name = "DC.Date" content = "<?PHP echo $post->post_date; ?>\n<?PHP
+			?><meta name = "DC.Creator" content = "<?PHP echo $user->user_nicename; ?>"><?PHP
+			?><meta name = "DC.Title" content = "<?PHP echo $post->post_title; ?><?PHP
+			?><meta name = "DC.Date" content = "<?PHP echo $post->post_date; ?><?PHP
 			foreach($cats as $subject){
-				?><meta name = "DC.Subject" content = "<?PHP echo $subject['name']; ?>">\n<?PHP
+				?><meta name = "DC.Subject" content = "<?PHP echo $subject['name']; ?>"><?PHP
 			}
 		}
 	?>
