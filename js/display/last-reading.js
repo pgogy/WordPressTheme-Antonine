f2fe73896e93ec79663d829c80e05599a77b17fb
patlockley <patrick.lockley@googlemail.com>
2016-03-09 14:06:52 +0000

    added delay till after first poll

--- js/display/last-reading.js
+++ js/display/last-reading.js
@@ -1,1 +1,10 @@
-Cookies.set("user_page_last_read_cookie", new Date().getTime());
+function antonine_last_read_cookie(){
+	console.log("cookie being set");
+	Cookies.set("user_page_last_read_cookie", new Date().getTime());	
+}
+
+jQuery(document).ready( function(){
+		console.log("cookie timer");
+		setTimeout(antonine_last_read_cookie, 10000);
+	}
+);
