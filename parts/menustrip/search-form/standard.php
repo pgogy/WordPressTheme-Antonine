5a9c34e4f2687cffbbc266bf6b6732c9feee4c5a
patlockley <patrick.lockley@googlemail.com>
2016-03-14 10:07:49 +0000

    Accessibility

--- parts/menustrip/search-form/standard.php
+++ parts/menustrip/search-form/standard.php
@@ -1,8 +1,8 @@
-<p id="searchClose" class="holderclose"><span><i class="fa fa-times"></i></span></p>
+<p id="searchClose" class="holderclose"><span><span class="fa fa-times"></span></span></p>
 <div id="searchform">
 	<form action="<?PHP echo home_url(); ?>" method="GET">
 		<label for="searchbox"><?PHP echo __("Search for", "antonine"); ?></label>
-		<input id="searchbox" type="text" class='antonine_search_box' name="s" value="<?PHP echo __("Enter term....", "antonine"); ?>" />
+		<input id="searchbox" type="text" maxlength="100" class='antonine_search_box' name="s" value="<?PHP echo __("Enter term....", "antonine"); ?>" />
 		<input type="submit" value="<?PHP echo __("Search", "antonine"); ?>" />
 	</form>
 </div>
