5a1d79468050cc72a3efafd239642304745de16e
patlockley <patrick.lockley@googlemail.com>
2016-03-14 10:05:28 +0000

    Spurious

--- sidebar-sidebar-right.php
+++
@@ -1,8 +0,0 @@
-<?php if ( is_active_sidebar( 'sidebar-right' ) ) { ?>
-<div id="sidebar-right" class="secondary">
-	<div id="widget-area-right" class="widget-area" role="complementary">
-		<?php dynamic_sidebar("sidebar-right"); ?>
-	</div><!-- .widget-area -->
-</div><!-- .secondary -->
-<?PHP } ?>
-
