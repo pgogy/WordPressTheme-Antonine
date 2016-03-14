474702e955ea2229181cbb731f565a6eebd7ead9
patlockley <patrick.lockley@googlemail.com>
2016-03-14 10:09:26 +0000

    Tabindex and extra links for screen readers

--- single.php
+++ single.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="site-main" role="main" tabindex="-1">
 		<?php
 		while ( have_posts() ) : the_post();
 		
@@ -17,7 +17,7 @@
 
 				?><div class="linkslower">
 					<div class="linkprevious"><?PHP previous_post_link() ?></div><div class="linknext"><?PHP next_post_link() ?></div>
-					<div class="linkhome"><a href="<?PHP site_url() ?>"><?PHP echo get_bloginfo('name'); ?> <?PHP echo __("home", "antonine"); ?></a></div>
+					<div class="linkhome"><a href="<?PHP echo site_url() ?>"><?PHP echo get_bloginfo('name'); ?> <?PHP echo __("home", "antonine"); ?></a></div>
 				</div><?PHP
 			
 			endif;
