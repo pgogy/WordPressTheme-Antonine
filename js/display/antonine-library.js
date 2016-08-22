7347454e1b15c2e1e761e77939188b14dc7749f9
patlockley <patrick.lockley@googlemail.com>
2016-08-22 13:46:07 +0100

    Solved IE bug, added new search function

--- js/display/antonine-library.js
+++ js/display/antonine-library.js
@@ -18,12 +18,25 @@ function antonine_aria(setting){
 
 function antonine_menu_slide(items){
 	if(items.length!=0){
-		item = items.shift();
-		jQuery(item)
+		article = items.shift();
+		jQuery(article)
 			.css("display","none")
 			.css("left","0");
-		jQuery(item)
+		jQuery(article)
 			.fadeIn(600, function(){antonine_menu_slide(items);});
+	}else{
+		scrolling = false;
+	}
+}
+
+function antonine_search_menu_slide(items){
+	if(items.length!=0){
+		article = items.shift();
+		jQuery(article)
+			.css("display","none")
+			.css("left","0");
+		jQuery(article)
+			.fadeIn(50, function(){antonine_search_menu_slide(items);});
 	}else{
 		scrolling = false;
 	}
@@ -31,8 +44,8 @@ function antonine_menu_slide(items){
 
 function antonine_menu_slide_filter(items){
 	if(items.length!=0){
-		item = items.shift();
-		jQuery(item)
+		article = items.shift();
+		jQuery(article)
 			.fadeIn(600, function(){antonine_menu_slide_filter(items);});
 	}else{
 		jQuery('#scroll_bottom').html("All posts are shown");
@@ -52,7 +65,7 @@ function antonine_article_align(){
 
 				leftpx = (parent_width - width)/2;
 				toppx = (parent_height - height)/2;
-				
+
 				jQuery(value)
 					.css("position", "relative")
 					.css("top", toppx + "px")
@@ -70,6 +83,51 @@ function antonine_article_align(){
 
 				leftpx = (parent_width - width)/2;
 				toppx = ((parent_height - height)/2);
+
+				leftpx = (parent_width - width)/2;
+				toppx = (parent_height - height)/2;
+
+				jQuery(value)
+					.css("position", "relative")
+					.css("top", toppx + "px")
+					.css("left", leftpx + "px");
+			}
+		);		
+}
+
+function antonine_search_align(){
+
+	jQuery( "article .home-align" )
+		.each(
+			function(index,value){
+				width = jQuery(value).width();
+				parent_width = jQuery(value).parent().width();
+				height = jQuery(value).height();
+				parent_height = jQuery(value).parent().parent().height();
+
+				leftpx = (parent_width - width)/2;
+				toppx = (parent_height - height)/2;
+				
+				jQuery(value)
+					.css("position", "relative")
+					.css("top", toppx + "px")
+					.css("left", leftpx + "px");
+			}
+		);
+		
+	jQuery( "article .home-align-title" )
+		.each(
+			function(index,value){
+				width = jQuery(value).width();
+				parent_width = jQuery(value).parent().width();
+				height = jQuery(value).height();
+				parent_height = jQuery(value).parent().parent().height();
+
+				leftpx = (parent_width - width)/2;
+				toppx = ((parent_height - height)/2);
+
+				leftpx = (parent_width - width)/2;
+				toppx = (parent_height - height)/2;
 
 				jQuery(value)
 					.css("position", "relative")
