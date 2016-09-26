918ab2f565715d43e4be4a4c0a1da070c0f9d41a
patlockley <patrick.lockley@googlemail.com>
2016-09-26 12:41:03 +0100

    Solved page loop issue

--- page.php
+++ page.php
@@ -12,16 +12,18 @@
  */
 
 get_header(); ?>
-
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
 		<?php
+
 		// Start the loop.
 		while ( have_posts() ) : the_post();
 
 			// Include the page content template.
-			get_template_part( 'content', 'page' );
+			get_template_part( 'parts/content/page' );
+
+			?><div class="links"><div class="linkprevious"><?PHP previous_post_link() ?></div><div class="linknext"><?PHP next_post_link() ?></div><div id="gradient"></div></div><?PHP
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
@@ -31,9 +33,6 @@ get_header(); ?>
 		// End the loop.
 		endwhile;
 		?>
-
 		</main><!-- .site-main -->
-		<?php get_sidebar( 'sidebar-right' ); ?>
 	</div><!-- .content-area -->
-
 <?php get_footer(); ?>
