5de12586ce9f2e4e2f85ed2556606734e4a5ab8c
patlockley <patrick.lockley@googlemail.com>
2016-11-15 15:26:23 +0000

    Removed files to move into plugins

--- js/display/last-reading.js
+++
@@ -1,8 +0,0 @@
-function antonine_last_read_cookie(){
-	Cookies.set("user_page_last_read_cookie", new Date().getTime());	
-}
-
-jQuery(document).ready( function(){
-		setTimeout(antonine_last_read_cookie, 10000);
-	}
-);
