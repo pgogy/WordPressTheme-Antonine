2936e81efac5651b3a5f0d334068698fee6d9c97
patlockley <patrick.lockley@googlemail.com>
2016-11-15 15:37:12 +0000

    Changed URLs

--- parts/menustrip/search-form/standard.php
+++ parts/menustrip/search-form/standard.php
@@ -1,6 +1,6 @@
 <p id="searchClose" class="holderclose"><span><span class="fa fa-times"></span></span></p>
 <div id="searchform">
-	<form action="<?PHP echo home_url(); ?>" method="GET">
+	<form action="<?PHP echo esc_url( home_url( '/' ) ) ?>" method="GET">
 		<label for="searchbox"><?PHP echo __("Search for", "antonine"); ?></label>
 		<input id="searchbox" type="text" maxlength="100" class='antonine_search_box' name="s" value="<?PHP echo __("Enter term....", "antonine"); ?>" />
 		<input type="submit" value="<?PHP echo __("Search", "antonine"); ?>" />
