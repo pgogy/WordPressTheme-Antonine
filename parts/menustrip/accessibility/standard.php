9f30841d640aa8020e7964a107aa3967077d1782
patlockley <patrick.lockley@googlemail.com>
2016-11-15 15:37:46 +0000

    esc_html and neatened

--- parts/menustrip/accessibility/standard.php
+++ parts/menustrip/accessibility/standard.php
@@ -6,20 +6,20 @@
 			<label for="basic_accessibility_background-color">
 				<?PHP echo __("Background Colour", "antonine"); ?>
 			</label>
-			<input name="background-colour" type='text' id="basic_accessibility_background-color" maxlength="10" title="<?PHP echo __("Background colour", "antonine"); ?>" />
-			<input type='hidden' name="basic_accessibility_background-color_default" id="basic_accessibility_background-color_default" value="<?PHP echo get_theme_mod('site_allsite_background_colour'); ?>" />
-			<button id="basic_accessibility_widget_restore_background">Restore</button>
+			<input name="background-colour" type='text' id="basic_accessibility_background-color" maxlength="10" title="<?PHP echo esc_html(__("Background colour", "antonine")); ?>" />
+			<input type='hidden' name="basic_accessibility_background-color_default" id="basic_accessibility_background-color_default" value="<?PHP echo esc_html(get_theme_mod('site_allsite_background_colour')); ?>" />
+			<button id="basic_accessibility_widget_restore_background"><?PHP echo __("Restore", "antonine"); ?></button>
 			<label for="basic_accessibility_color">
 				<?PHP echo __("Text Colour", "antonine"); ?>
 			</label>
-			<input name="text-colour" type='text' title="<?PHP echo __("Text colour", "antonine"); ?>" id="basic_accessibility_color" maxlength="10" />
-			<input type='hidden' name="basic_accessibility_color_default" id="basic_accessibility_color_default" value="<?PHP echo get_theme_mod('site_alltext_colour'); ?>" />
+			<input name="text-colour" type='text' title="<?PHP echo esc_html(__("Text colour", "antonine")); ?>" id="basic_accessibility_color" maxlength="10" />
+			<input type='hidden' name="basic_accessibility_color_default" id="basic_accessibility_color_default" value="<?PHP echo esc_html(get_theme_mod('site_alltext_colour')); ?>" />
 			<button id="basic_accessibility_widget_restore_colour"><?PHP echo __("Restore", "antonine"); ?></button>
 			<label for="basic_accessibility_font-size_default">
 				<?PHP echo __("Text Size", "antonine"); ?> : 
 			</label>
 			<input type='hidden' name="basic_accessibility_font-size_default" id="basic_accessibility_font-size_default" value="14px" />
-			<button id="basic_accessibility_widget_font-size_restore">Restore</button>
+			<button id="basic_accessibility_widget_font-size_restore"><?PHP echo __("Restore", "antonine"); ?></button>
 			<span id="basic_accessibility_font-size_display"></span>
 			<div id="access-font-size">
 			</div>
