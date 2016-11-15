487cb38af16f37a52e5619af0a9257bd6eef844d
patlockley <patrick.lockley@googlemail.com>
2016-11-15 15:31:51 +0000

    Changed urls

--- parts/search/search-form.php
+++ parts/search/search-form.php
@@ -1,7 +1,7 @@
 <div id="searchform">
-	<form action="<?PHP echo home_url(); ?>" method="GET">
+	<form action="<?PHP echo esc_url( home_url( '/' ) ) ?>" method="GET">
 		<label for="searchbox"><?PHP echo __("Search for", "antonine"); ?></label>
-		<input id="searchbox" type="text" maxlength="100" class='antonine_search_box' name="s" value="<?PHP echo __("Enter term....", "antonine"); ?>" />
+		<input id="searchbox" type="text" maxlength="100" class='antonine_search_box' name="s" value="<?PHP echo esc_html(__("Enter term....", "antonine")); ?>" />
 		<input type="submit" value="<?PHP echo __("Search", "antonine"); ?>" />
 	</form>
 </div>
