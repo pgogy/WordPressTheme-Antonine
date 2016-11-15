4cd18ad10aae1cc8f24eb59b059c3581a88d3ad1
patlockley <patrick.lockley@googlemail.com>
2016-11-15 15:38:08 +0000

    removed code which will move to plugins

--- parts/header/menu/standard.php
+++ parts/header/menu/standard.php
@@ -1,10 +1,10 @@
 `<?PHP
-	if(get_theme_mod("info")=="on"){
+	if(get_theme_mod("antonine[info]","on")=="on"){
 ?>
 <a><div id="infoShow"><span class="fa fa-info"></span></div></a>
 <?PHP
 	}
-	if(get_theme_mod("menu")=="on"){
+	if(get_theme_mod("antonine[menu]","on")=="on"){
 		if ( has_nav_menu( "primary" ) ) {
 			
 			?>
@@ -15,51 +15,36 @@
 		
 		}
 	}
-	if(get_theme_mod("share")=="on"){
-?>
-<a><div id="shareShow"><span class="fa fa-share-alt"></span></div></a>
-<?PHP
-	}
-	if(get_theme_mod("search")=="on"){
+	if(get_theme_mod("antonine[search]","on")=="on"){
 ?>
 <a><div id="searchShow"><span class="fa fa-search"></span></div></a>
 <?PHP
 	}
-	if(get_theme_mod("updates")=="on"){
-?>
-<a><div id="updatesShow"><span class="fa fa-globe"></span></div></a>
-<?PHP
-	}
-	if(get_theme_mod("filters")=="on"){
+	if(get_theme_mod("antonine[filters]","on")=="on"){
 		if(is_home()){
 ?>
 <a><div id="filterShow"><span class="fa fa-sliders"></span></div></a>
 <?PHP
 		}
 	}
-	if(get_theme_mod("comments")=="on"){
+	if(get_theme_mod("antonine[comments]","on")=="on"){
 ?>
 <a><div id="commmentsShow"><span class="fa fa-commenting"></span></div></a>
 <?PHP
 	}
-	if(get_theme_mod("widgets")=="on"){
+	if(get_theme_mod("antonine[widgets]","on")=="on"){
 ?>
 <a><div id="widgetsShow"><span class="fa fa-sitemap"></span></div></a>
 <?PHP
 	}
-	if(get_theme_mod("files")=="on"){
+	if(get_theme_mod("antonine[files]","on")=="on"){
 ?>
 <a><div id="filesShow"><span class="fa fa-folder"></span></div></a>
 <?PHP
 	}
-	if(get_theme_mod("accessibility")=="on"){
+	if(get_theme_mod("antonine[accessibility]","on")=="on"){
 ?>
 <a><div id="accessShow"><span class="fa fa-eye"></span></div></a>
-<?PHP
-	}
-	if(get_theme_mod("subscribe")=="on"){
-?>
-<a><div id="subscribeShow"><span class="fa fa-envelope"></span></div></a>
 <?PHP
 	}
 ?>
