f390a967dcdba9f9786a6962ea12afac3285fae7
patlockley <patrick.lockley@googlemail.com>
2016-11-15 15:36:52 +0000

    Removed google fonts

--- parts/header/main.php
+++ parts/header/main.php
@@ -3,5 +3,5 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<link href='//fonts.googleapis.com/css?family=Dosis:300italic,400italic,600italic,700italic,800italic,400,800,700,600,300&subset=latin,cyrillic-ext,latin-ext,cyrillic,greek-ext,vietnamese' rel='stylesheet' type='text/css'>
-	<link href='//fonts.googleapis.com/css?family=Josefin+Sans:400' rel='stylesheet' type='text/css'>
+	<?php wp_head(); ?>
+</head>
