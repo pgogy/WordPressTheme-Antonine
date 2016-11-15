5cf6d4c2f4b27f867914337fd90e4a95dcca982a
patlockley <patrick.lockley@googlemail.com>
2016-11-15 15:26:45 +0000

    Neatened

--- footer.php
+++ footer.php
@@ -1,13 +1,13 @@
-	</div><!-- .site-content -->
-	<?PHP
-		get_template_part("parts/footer/scroll_up");
+		</div>
+		<?PHP
+			get_template_part("parts/footer/scroll_up");
+		?>
+		<footer id="colophon" class="site-footer" role="contentinfo">
+		</footer>
+	</div>
+	<?php 
+		get_template_part("parts/footer/menus");
+		wp_footer(); 
 	?>
-	<footer id="colophon" class="site-footer" role="contentinfo">
-	</footer><!-- .site-footer -->
-</div><!-- .site -->
-<?php 
-	get_template_part("parts/footer/menus");
-	wp_footer(); 
-?>
-</body>
+	</body>
 </html>
