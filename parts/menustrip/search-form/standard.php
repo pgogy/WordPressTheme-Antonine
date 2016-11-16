6a05f98c784350f27861c34d96e5c37448229167
patlockley <patrick.lockley@googlemail.com>
2016-11-16 21:59:55 +0000

    Renamed searchform for accessibility

--- parts/menustrip/search-form/standard.php
+++ parts/menustrip/search-form/standard.php
@@ -1,5 +1,5 @@
 <p id="searchClose" class="holderclose"><span><span class="fa fa-times"></span></span></p>
-<div id="searchform">
+<div id="sitesearchform">
 	<form action="<?PHP echo esc_url( home_url( '/' ) ) ?>" method="GET">
 		<label for="searchbox"><?PHP echo __("Search for", "antonine"); ?></label>
 		<input id="searchbox" type="text" maxlength="100" class='antonine_search_box' name="s" value="<?PHP echo __("Enter term....", "antonine"); ?>" />
