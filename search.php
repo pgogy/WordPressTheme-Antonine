adecaad4b77e6ed8032fb80ab39dbf11ada18c07
patlockley <patrick.lockley@googlemail.com>
2016-11-29 21:02:53 +0000

    reworked query scroll

--- search.php
+++ search.php
@@ -7,16 +7,15 @@
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main"><?PHP
 
-			query_posts( $query_string . '&posts_per_page=-1' );
-
 			while ( have_posts() ) : the_post();
 
 				get_template_part( 'parts/content/content-all', get_post_format() );
 
 			endwhile;
 	
+			wp_reset_postdata();
+	
 		?></main>
-		<div id="scroll_bottom" scroll="on" page="<?PHP echo $posts_total; ?>" total_posts="<?PHP echo $posts_total; ?>"></div>
 	</div>
 <?PHP
 	}else{
